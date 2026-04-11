650ba6334d4e23a6f8ac1b2b0950f1701515fe5f
antarixxi <ashish.gusani8@gmail.com>
2026-04-11 17:33:16 +0530

    HomePage UI: - Pages   - Home Page - Theme   - Colors - Widgets   - Search Bar Button   - Search Section   - Side Bar Button   - Side Bar

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:godparticle2/theme/colors.dart';
+import 'package:godparticle2/widgets/search_section.dart';
+import 'package:godparticle2/widgets/side_bar.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          // side navbar
+          SideBar(),
+          Expanded(
+            child: Column(
+              children: [
+                // search section
+                Expanded(child: SearchSection()),
+                // footer
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  child: Wrap(
+                    alignment: WrapAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsetsGeometry.symmetric(horizontal: 12),
+                        child: Text(
+                          "Pro",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsGeometry.symmetric(horizontal: 12),
+                        child: Text(
+                          "Enterprise",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsGeometry.symmetric(horizontal: 12),
+                        child: Text(
+                          "Store",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsGeometry.symmetric(horizontal: 12),
+                        child: Text(
+                          "Blog",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsGeometry.symmetric(horizontal: 12),
+                        child: Text(
+                          "Careers",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsGeometry.symmetric(horizontal: 12),
+                        child: Text(
+                          "English (English)",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
